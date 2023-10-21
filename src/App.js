8d402177f607efe0a393ccbb6310e44080024bea
aboobacker <saboobacker189@gmail.com>
2023-10-22 00:51:44 +0530

    add cards almost completad the design

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import NavBars from "./componants/navbar/NavBars";
 import './App.css'
 import Banner from "./Banner/Banner";
+import RowPoster from "./rowposter/RowPoster";
 function App() {
   return (
     <div className="App">
       <NavBars></NavBars>
       <Banner></Banner>
+      <RowPoster></RowPoster>
 
 
     </div>
