7a5539560cae4c35fc6a2b8e95234bd944d4b39e
aboobacker <saboobacker189@gmail.com>
2023-10-21 20:18:38 +0530

    add navbar,banner

---
+++ src/Banner/Banner.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Banner.css'
+
+function Banner() {
+    return (
+        <div className='banner'>
+            <div className='content'>
+                <h1 className='title'>jjjjjjjjjjjj</h1>
+                <div className="buttons">
+                    <button className='button'>play</button>
+                    <button className='button'>mylist</button>
+
+                </div>
+                <h1 className='description'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet aspernatur consequatur, magni ex qui accusantium impedit non inventore magnam vitae nemo alias? Eveniet reiciendis voluptatum quia quos repudiandae ratione sit.</h1>
+            </div>
+
+        </div>
+    )
+}
+
+export default Banner
