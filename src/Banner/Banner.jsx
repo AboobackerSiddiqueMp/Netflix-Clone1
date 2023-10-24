988202bd97f747d0dfdda13bee0fd3b23626460f
aboobacker <saboobacker189@gmail.com>
2023-10-24 20:07:42 +0530

    add tmdb api.and done first phase adding data from api,add title img p in banner

--- src/Banner/Banner.jsx
+++ src/Banner/Banner.jsx
@@ -1,17 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Banner.css'
 
+import axios from '../Axios'
+import {API_KEY,imgUrl} from '../constants/constant'
+
+
 function Banner() {
+    const[movie,setmovie]=useState()
+    useEffect(() => {
+        axios.get(`trending/movie/day?api_key=${API_KEY}`).then((response)=>{
+            console.log(response.data.results[0])
+            setmovie(response.data.results[0])
+        })
+    .catch((error)=>{
+        console.log('error fech data',error)
+
+    });
+
+     
+    }, []);
+    
     return (
-        <div className='banner'>
+        <div style={{backgroundImage:`url(${movie? imgUrl+movie.backdrop_path:""})`}}    className='banner'>
             <div className='content'>
-                <h1 className='title'>money heist</h1>
+                <h1 className='title'>{movie? movie.title:""}</h1>
                 <div className="buttons">
                     <button className='button'>play</button>
                     <button className='button'>mylist</button>
 
                 </div>
-                <h1 className='description'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod accusamus reiciendis voluptatum. Sequi neque ex non quis veniam atque dicta temporibus ullam modi perspiciatis provident quasi dolorem, deserunt quas? Magnam?
+                <h1 className='description'>{movie? movie.overview:""}
                 </h1>
             </div>
             <div className="fade"></div>
---
+++ src/constants/constant.jsx
@@ -0,0 +1,3 @@
+export const baseUrl='https://api.themoviedb.org/3';
+export const API_KEY="91edbdebaee4bd43382797933f35ad32";
+export const imgUrl='https://image.tmdb.org/t/p/original'
